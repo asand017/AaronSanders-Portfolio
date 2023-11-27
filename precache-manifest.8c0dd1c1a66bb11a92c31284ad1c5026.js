4aa0f0493479faba7db4050f7bd16ffd002c40f6
aaron (kb) <aaron.sanders342@gmail.com>
2023-11-27 15:54:53 -0600

    Updates

---
+++ precache-manifest.8c0dd1c1a66bb11a92c31284ad1c5026.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b14c3cf010c21d491d606f615ca1353",
+    "url": "/asand017/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "08b009125197f0bda136",
+    "url": "/asand017/react-frontend-dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "c60a293423ffad19b434",
+    "url": "/asand017/react-frontend-dev-portfolio/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "08b009125197f0bda136",
+    "url": "/asand017/react-frontend-dev-portfolio/static/js/2.0291696b.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/asand017/react-frontend-dev-portfolio/static/js/2.0291696b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c60a293423ffad19b434",
+    "url": "/asand017/react-frontend-dev-portfolio/static/js/main.66487abc.chunk.js"
+  },
+  {
+    "revision": "ba68b5c842d85bd4cfc5",
+    "url": "/asand017/react-frontend-dev-portfolio/static/js/runtime-main.24af5ebc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-frontend-dev-portfolio/precache-manifest.ee50bc0a1fbb4aafab17a8b6801d6718.js"
+  "/asand017/react-frontend-dev-portfolio/precache-manifest.8c0dd1c1a66bb11a92c31284ad1c5026.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-frontend-dev-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/asand017/react-frontend-dev-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
